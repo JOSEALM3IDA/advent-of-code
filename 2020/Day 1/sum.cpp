f2f98f64ef180b2067dda41843044a0fd3c47200
JOSEALM3IDA <jfeaisec@gmail.com>
2020-12-01 19:47:44 +0000

    Day 1 & README

---
+++ 2020/Day 1/sum.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+#define NUMBER_TO_FIND 2020
+
+using namespace std;
+
+vector<int> readFile(string fileName) {
+    ifstream inputFile;
+    vector<int> input;
+
+    inputFile.open(fileName);
+    if (inputFile.is_open()) {
+        string line;
+        while (getline(inputFile, line)) {
+            input.push_back(atoi(line.c_str()));
+        }
+        inputFile.close();
+    }
+
+    return input;
+}
+
+int main1() {
+    vector<int> input = readFile("input.txt");
+
+    for(int n1 = 0; n1 < input.size(); n1++)
+        for (int n2 = n1 + 1; n2 < input.size(); n2++)
+            if (input[n1] + input[n2] == NUMBER_TO_FIND) {
+                cout << "Numbers: " << input[n1] << " and " << input[n2] << endl;
+                cout << "Multiplied: " << input[n1] * input[n2] << endl;
+                return 1;
+            }
+    
+    cout << "Not found :/" << endl;
+    return 0;
+}
+
+int main2() {
+    vector<int> input = readFile("input.txt");
+
+    for(int n1 = 0; n1 < input.size(); n1++)
+        for (int n2 = n1 + 1; n2 < input.size(); n2++)
+            for (int n3 = n2 + 1; n3 < input.size(); n3++)
+                if (input[n1] + input[n2] + input[n3] == NUMBER_TO_FIND) {
+                    cout << "Numbers: " << input[n1] << " and " << input[n2] << " and " << input[n3] << endl;
+                    cout << "Multiplied: " << input[n1] * input[n2] * input[n3] << endl;
+                    return 1;
+                }
+    
+    cout << "Not found :/" << endl;
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# advent-of-code
+```c++
+#ifndef ADVENT_OF_CODE_H
+#define ADVENT_OF_CODE_H
+
+#define YEAR 2020
+
+void wishes() { return "Merry Christmas and a Happy New Year!"; };
+
+#endif /* ADVENT_OF_CODE_H */
+```
+
+### Join my private leaderboard! `1050747-318b1fcb`
+
+This repository will be used to store my solution to all the puzzles using C++ only.
+
+---
+
+### [About - Advent of Code](https://adventofcode.com/about)
