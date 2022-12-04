3298868220e0a15a4267c066f27f77acfbcf9138
JOSEALM3IDA <josealmeida.dev@gmail.com>
2022-12-04 06:36:46 +0100

    Improve Day 4 2022

--- 2022/src/pt/josealm3ida/aoc2022/day4/CampCleanup.java
+++ 2022/src/pt/josealm3ida/aoc2022/day4/CampCleanup.java
@@ -16,18 +16,23 @@ public class CampCleanup {
         return ( range1[0] <= range2[0] && range1[1] >= range2[0] ) || ( range1[0] <= range2[1] && range1[1] >= range2[1] );
     }
 
+    public static int[][] getRangesFromStr(String s) {
+        String[] ranges = s.split(",");
+
+        String[] range1str = ranges[0].split("-");
+        String[] range2str = ranges[1].split("-");
+
+        int[] range1 = { Integer.parseInt(range1str[0]), Integer.parseInt(range1str[1]) };
+        int[] range2 = { Integer.parseInt(range2str[0]), Integer.parseInt(range2str[1]) };
+
+        return new int[][]{range1, range2};
+    }
+
     public static int getNContainedRanges(String[] input) {
         int nContained = 0;
         for (String s : input) {
-            String[] ranges = s.split(",");
-
-            String[] range1str = ranges[0].split("-");
-            String[] range2str = ranges[1].split("-");
-
-            int[] range1 = { Integer.parseInt(range1str[0]), Integer.parseInt(range1str[1]) };
-            int[] range2 = { Integer.parseInt(range2str[0]), Integer.parseInt(range2str[1]) };
-
-            if (isContained(range1, range2) || isContained(range2, range1)) nContained++;
+            int[][] ranges = getRangesFromStr(s);
+            if (isContained(ranges[0], ranges[1]) || isContained(ranges[1], ranges[0])) nContained++;
         }
 
         return nContained;
@@ -36,15 +41,8 @@ public class CampCleanup {
     public static int getNOverlappedRanges(String[] input) {
         int nOverlapped = 0;
         for (String s : input) {
-            String[] ranges = s.split(",");
-
-            String[] range1str = ranges[0].split("-");
-            String[] range2str = ranges[1].split("-");
-
-            int[] range1 = { Integer.parseInt(range1str[0]), Integer.parseInt(range1str[1]) };
-            int[] range2 = { Integer.parseInt(range2str[0]), Integer.parseInt(range2str[1]) };
-
-            if (hasOverlap(range1, range2) || hasOverlap(range2, range1)) nOverlapped++;
+            int[][] ranges = getRangesFromStr(s);
+            if (hasOverlap(ranges[0], ranges[1]) || hasOverlap(ranges[1], ranges[0])) nOverlapped++;
         }
 
         return nOverlapped;
